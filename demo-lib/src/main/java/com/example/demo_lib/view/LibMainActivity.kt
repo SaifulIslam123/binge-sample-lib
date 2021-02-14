60050f51c0e6498d8b2c950e10c972c70ea1931f
MD. Saiful Islam <saiful.islam@reddotdigitalit.com>
2021-02-15 02:24:59 +0600

    lib add to app module

---
+++ demo-lib/src/main/java/com/example/demo_lib/view/LibMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demo_lib.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.demo_lib.R
+
+class LibMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lib_main)
+    }
+}
